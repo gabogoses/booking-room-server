ec54b669ef9751907cba9894ea084fe6dda929fc
Gabo Goses <gabrielmargoses@gmail.com>
2022-01-15 15:10:37 +0100

    feature: create user signup method

--- src/graphql/resolvers/user.js
+++ src/graphql/resolvers/user.js
@@ -9,8 +9,23 @@ const eventResolvers = {
         },
     },
     Mutation: {
-        signup: async (_, {}, { models }) => {
-            return 'READY';
+        signup: async (_, { email, password }, { models }) => {
+            if (!email || !password) {
+                throw new Error('Invalid user inputs');
+            }
+
+            try {
+                const newUser = new models.User({ email, password });
+                await newUser.save();
+
+                const { _id: userId } = newUser;
+                const token = signToken(userId);
+
+                return { token };
+            } catch (err) {
+                console.error('An error occured', err.message);
+                throw new ApolloError(err);
+            }
         },
         login: async (_, {}, { models }) => {
             return 'READY';
