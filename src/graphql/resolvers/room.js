066d3c9621762bee232986a54466de26f5e4f551
Gabo Goses <gabrielmargoses@gmail.com>
2022-01-15 15:35:02 +0100

    feature: adding getRooms methods

--- src/graphql/resolvers/room.js
+++ src/graphql/resolvers/room.js
@@ -5,7 +5,12 @@ const { ApolloError, AuthenticationError } = require('apollo-server');
 const roomResolvers = {
     Query: {
         getRooms: async (_, {}, { models }) => {
-            return 'READY';
+            try {
+                return models.Room.find({}).populate('events');
+            } catch (err) {
+                console.error('An error occured', err.message);
+                throw new ApolloError(err);
+            }
         },
         getRoom: async (_, { id }, { models }) => {
             return 'READY';
