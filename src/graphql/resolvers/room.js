6eaad4d0f30fba1fdfe084b5140f931a9b7459dc
Gabo Goses <gabrielmargoses@gmail.com>
2022-01-15 15:36:40 +0100

    feature: adding createRoom method on resolvers

--- src/graphql/resolvers/room.js
+++ src/graphql/resolvers/room.js
@@ -22,8 +22,24 @@ const roomResolvers = {
         },
     },
     Mutation: {
-        createRoom: async (_, { roomNumber }, { models }) => {
-            return 'READY';
+        createRoom: async (_, { roomNumber }, { isAdmin, isAuthenticated, models }) => {
+            if (!isAuthenticated && !isAdmin) {
+                throw new AuthenticationError('User is not allowed to create this ressource');
+            }
+
+            if (!roomNumber) {
+                throw new Error('Invalid user input');
+            }
+
+            try {
+                const newRoom = new models.Room({ roomNumber });
+                await newRoom.save();
+
+                return newRoom;
+            } catch (err) {
+                console.error('An error occured', err.message);
+                throw new ApolloError(err);
+            }
         },
     },
 };
