cb5c03ce01a08dad91104d44fcbba0c3b9fd35ce
Gabo Goses <gabrielmargoses@gmail.com>
2022-01-15 22:00:48 +0100

    fix: remove useless file

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,9 @@
 const decodeToken = require('./decodeToken');
-const getEventEndTime = require('./getEventEndTime');
 const sendMail = require('./sendMail');
 const signToken = require('./signToken');
 
 module.exports = {
     decodeToken,
-    getEventEndTime,
     sendMail,
     signToken,
 };
