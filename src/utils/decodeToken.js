25d8cd392f82a9134506c267ff0747fbbf03c0a3
Gabo Goses <gabrielmargoses@gmail.com>
2022-01-15 15:15:06 +0100

    feature: create decode JWT token method

---
+++ src/utils/decodeToken.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { verify } = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const decodeToken = (token) => {
+    try {
+        const payload = verify(token, JWT_SECRET);
+        const { id, isAdmin } = payload;
+        return { id, isAdmin, isAuthenticated: true };
+    } catch (err) {
+        console.error('An error occured', err.message);
+        return { isAuthenticated: false };
+    }
+};
+
+module.exports = decodeToken;
